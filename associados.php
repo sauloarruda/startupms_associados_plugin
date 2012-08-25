b87dd4baa5674930921bb250d21bffbcdd2acb59
Saulo Arruda <sauloarruda@gmail.com>
2012-08-24 22:22:57 -0400

    form de startups

--- associados.php
+++ associados.php
@@ -76,7 +76,8 @@ function startups_options() {
 	if ($_POST['id']) {
 		// gravar...
 	} else {
-		$form = ($_GET['startup_id']) ? get_startup($id) : array();
+		$form = ($_GET['startup_id']) ? get_startup($id) : 
+			array('id' => 1, 'email' => 'contato@starspremium.com.br', 'nome' => 'StarsPremium', 'endereco' => 'Rua Teldo Kasper, 109', 'cidade' => 0, 'elevator_pitch' => 'STARSPREMIUM é uma agência de incentivo e relacionamento, nova e diferente. Jovem, cheia de energia e inovadora.', 'estagio' => 2, 'segmento' => '3,10,Incentivo e fidelização', 'fundacao' => new DateTime('2011-08-20'), 'startup_ajuda' => 2, 'url' => 'http://starspremium.com.br', 'facebook' => '', 'twitter' => 'starspremium', 'logomarca' => '', 'nome_socio1' => 'Saulo Arruda', 'telefone_socio1' => '(67) 9238-4909', 'email_socio1' => 'saulo@starspremium.com.br', 'formacao_socio1' => 'Melhoria do Processo de Software', 'nome_socio2' => 'Jefferson Moreira', 'telefone_socio2' => '(67) 9257-7444', 'email_socio2' => 'jeffmor@starspremium.com.br', 'formacao_socio2' => 'Ciência da Computação', 'nome_socio3' => 'Flávio Antunes', 'telefone_socio3' => '(11) 98882-0202', 'email_socio3' => 'flavio@starspremium.com.br', 'formacao_socio3' => 'Publicidade e Propaganda');
 		include('startups_form.php');
 	}
 }
@@ -86,4 +87,45 @@ function save_startups($form)
 	
 }
 
+function get_cidades() {
+	return array(
+		array(0, 'Campo Grande'),
+		array(1, 'Dourados'),
+		array(2, 'Três Lagoas')
+	);
+}
+
+function get_estagios() {
+	return array(
+		array(0, 'Pré-modelo - ainda estamos tentando encontrar um modelo para ter receita'),
+		array(1, 'Pré-receita - já encontramos um modelo e estamos quase tendo receita'),
+		array(2, 'Pré-lucro - já temos receita e estamos caminhando para que ela pague toda a nossa operação')
+	);
+}
+
+function get_segmentos() {
+	return array(
+		array(0, 'E-commerce'),
+		array(1, 'Mobile'),
+		array(2, 'Vídeos'),
+		array(3, 'Serviços'),
+		array(4, 'Web'),
+		array(5, 'Crowdsourcing'),
+		array(6, 'Games'),
+		array(7, 'Cultura'),
+		array(8, 'Educação'),
+		array(9, 'Saúde'),
+		array(10, 'Outro:')
+	);
+}
+
+function get_startup_ajudas() {
+	return array(
+		array(0, 'Começar meu negócio (tirar do papel, validar e encontrar sócios/fornecedores)'),
+		array(1, 'Apoio na gestão do meu negócio (capacitação, mentoria, consultoria, incubação e aceleração)'),
+		array(2, 'Apoio comercial (vendas e divulgação do meu negócio)'),
+		array(3, 'Apoio na captação de recursos para o meu negócio (projetos públicos e investidores)'),
+		array(4, 'Outro')
+		);	
+}
 ?>
